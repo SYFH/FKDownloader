c5fbf19c461d636f847cd42c9ae4a5d4632c935c
norld <syfh@live.com>
2018-11-24 19:56:20 +0800

    简单添加网络监控

---
+++ FKDownloader/FKReachability.h
@@ -0,0 +1,50 @@
+//
+//  FKReachability.h
+//  FKDownloaderDemo
+//
+//  Created by Norld on 2018/11/24.
+//  Copyright © 2018 Norld. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, NetworkStatus) {
+    NotReachable = 0,
+    ReachableViaWiFi,
+    ReachableViaWWAN
+};
+
+extern NSString *kReachabilityChangedNotification;
+
+@interface FKReachability : NSObject
+
+
+/*!
+ * Use to check the reachability of a given host name.
+ */
++ (instancetype)reachabilityWithHostName:(NSString *)hostName;
+
+/*!
+ * Use to check the reachability of a given IP address.
+ */
++ (instancetype)reachabilityWithAddress:(const struct sockaddr *)hostAddress;
+
+/*!
+ * Checks whether the default route is available. Should be used by applications that do not connect to a particular host.
+ */
++ (instancetype)reachabilityForInternetConnection;
+
+/*!
+ * Start listening for reachability notifications on the current run loop.
+ */
+- (BOOL)startNotifier;
+- (void)stopNotifier;
+
+- (NetworkStatus)currentReachabilityStatus;
+
+/*!
+ * WWAN may be available, but not active until a connection has been established. WiFi may require a connection for VPN on Demand.
+ */
+- (BOOL)connectionRequired;
+
+@end
---
+++ FKDownloader/FKReachability.m
@@ -0,0 +1,191 @@
+//
+//  FKReachability.m
+//  FKDownloaderDemo
+//
+//  Created by Norld on 2018/11/24.
+//  Copyright © 2018 Norld. All rights reserved.
+//
+
+#import "FKReachability.h"
+
+#import <arpa/inet.h>
+#import <ifaddrs.h>
+#import <netdb.h>
+#import <sys/socket.h>
+#import <netinet/in.h>
+#import <SystemConfiguration/SystemConfiguration.h>
+#import <netinet/in.h>
+#import <CoreFoundation/CoreFoundation.h>
+
+NSString *kReachabilityChangedNotification = @"kNetworkReachabilityChangedNotification";
+
+#define kShouldPrintReachabilityFlags 1
+
+static void PrintReachabilityFlags(SCNetworkReachabilityFlags flags, const char *comment) {
+#if kShouldPrintReachabilityFlags
+    NSLog(@"Reachability Flag Status: %c%c %c%c%c%c%c%c%c %s\n",
+          (flags & kSCNetworkReachabilityFlagsIsWWAN)                ? 'W' : '-',
+          (flags & kSCNetworkReachabilityFlagsReachable)            ? 'R' : '-',
+          
+          (flags & kSCNetworkReachabilityFlagsTransientConnection)  ? 't' : '-',
+          (flags & kSCNetworkReachabilityFlagsConnectionRequired)   ? 'c' : '-',
+          (flags & kSCNetworkReachabilityFlagsConnectionOnTraffic)  ? 'C' : '-',
+          (flags & kSCNetworkReachabilityFlagsInterventionRequired) ? 'i' : '-',
+          (flags & kSCNetworkReachabilityFlagsConnectionOnDemand)   ? 'D' : '-',
+          (flags & kSCNetworkReachabilityFlagsIsLocalAddress)       ? 'l' : '-',
+          (flags & kSCNetworkReachabilityFlagsIsDirect)             ? 'd' : '-',
+          comment
+          );
+#endif
+}
+
+
+static void ReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkReachabilityFlags flags, void *info) {
+#pragma unused (target, flags)
+    NSCAssert(info != NULL, @"info was NULL in ReachabilityCallback");
+    NSCAssert([(__bridge NSObject*) info isKindOfClass: [FKReachability class]], @"info was wrong class in ReachabilityCallback");
+    
+    FKReachability *noteObject = (__bridge FKReachability *)info;
+    // Post a notification to notify the client that the network reachability changed.
+    [[NSNotificationCenter defaultCenter] postNotificationName: kReachabilityChangedNotification object: noteObject];
+}
+
+@interface FKReachability ()
+
+@property (nonatomic, assign) SCNetworkReachabilityRef reachabilityRef;
+
+@end
+
+@implementation FKReachability
+
++ (instancetype)reachabilityWithHostName:(NSString *)hostName {
+    FKReachability *returnValue = NULL;
+    SCNetworkReachabilityRef reachability = SCNetworkReachabilityCreateWithName(NULL, [hostName UTF8String]);
+    if (reachability != NULL) {
+        returnValue= [[self alloc] init];
+        if (returnValue != NULL) {
+            returnValue.reachabilityRef = reachability;
+        }
+        else {
+            CFRelease(reachability);
+        }
+    }
+    return returnValue;
+}
+
++ (instancetype)reachabilityWithAddress:(const struct sockaddr *)hostAddress {
+    SCNetworkReachabilityRef reachability = SCNetworkReachabilityCreateWithAddress(kCFAllocatorDefault, hostAddress);
+    FKReachability *returnValue = NULL;
+    if (reachability != NULL) {
+        returnValue = [[self alloc] init];
+        if (returnValue != NULL) {
+            returnValue.reachabilityRef = reachability;
+        }
+        else {
+            CFRelease(reachability);
+        }
+    }
+    return returnValue;
+}
+
++ (instancetype)reachabilityForInternetConnection {
+    struct sockaddr_in zeroAddress;
+    bzero(&zeroAddress, sizeof(zeroAddress));
+    zeroAddress.sin_len = sizeof(zeroAddress);
+    zeroAddress.sin_family = AF_INET;
+    
+    return [self reachabilityWithAddress: (const struct sockaddr *) &zeroAddress];
+}
+
+#pragma mark - Start and stop notifier
+- (BOOL)startNotifier {
+    BOOL returnValue = NO;
+    SCNetworkReachabilityContext context = {0, (__bridge void *)(self), NULL, NULL, NULL};
+    
+    if (SCNetworkReachabilitySetCallback(self.reachabilityRef, ReachabilityCallback, &context)) {
+        if (SCNetworkReachabilityScheduleWithRunLoop(self.reachabilityRef, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode)) {
+            returnValue = YES;
+        }
+    }
+    
+    return returnValue;
+}
+
+- (void)stopNotifier {
+    if (self.reachabilityRef != NULL) {
+        SCNetworkReachabilityUnscheduleFromRunLoop(self.reachabilityRef, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+    }
+}
+
+- (void)dealloc {
+    [self stopNotifier];
+    if (self.reachabilityRef != NULL) {
+        CFRelease(self.reachabilityRef);
+    }
+}
+
+#pragma mark - Network Flag Handling
+- (NetworkStatus)networkStatusForFlags:(SCNetworkReachabilityFlags)flags {
+    PrintReachabilityFlags(flags, "networkStatusForFlags");
+    if ((flags & kSCNetworkReachabilityFlagsReachable) == 0) {
+        // The target host is not reachable.
+        return NotReachable;
+    }
+    
+    NetworkStatus returnValue = NotReachable;
+    
+    if ((flags & kSCNetworkReachabilityFlagsConnectionRequired) == 0) {
+        /*
+         If the target host is reachable and no connection is required then we'll assume (for now) that you're on Wi-Fi...
+         */
+        returnValue = ReachableViaWiFi;
+    }
+    
+    if ((((flags & kSCNetworkReachabilityFlagsConnectionOnDemand ) != 0) ||
+         (flags & kSCNetworkReachabilityFlagsConnectionOnTraffic) != 0)) {
+        /*
+         ... and the connection is on-demand (or on-traffic) if the calling application is using the CFSocketStream or higher APIs...
+         */
+        
+        if ((flags & kSCNetworkReachabilityFlagsInterventionRequired) == 0) {
+            /*
+             ... and no [user] intervention is needed...
+             */
+            returnValue = ReachableViaWiFi;
+        }
+    }
+    
+    if ((flags & kSCNetworkReachabilityFlagsIsWWAN) == kSCNetworkReachabilityFlagsIsWWAN) {
+        /*
+         ... but WWAN connections are OK if the calling application is using the CFNetwork APIs.
+         */
+        returnValue = ReachableViaWWAN;
+    }
+    
+    return returnValue;
+}
+
+- (BOOL)connectionRequired {
+    NSAssert(self.reachabilityRef != NULL, @"connectionRequired called with NULL reachabilityRef");
+    SCNetworkReachabilityFlags flags;
+    
+    if (SCNetworkReachabilityGetFlags(self.reachabilityRef, &flags)) {
+        return (flags & kSCNetworkReachabilityFlagsConnectionRequired);
+    }
+    
+    return NO;
+}
+
+- (NetworkStatus)currentReachabilityStatus {
+    NSAssert(self.reachabilityRef != NULL, @"currentNetworkStatus called with NULL SCNetworkReachabilityRef");
+    NetworkStatus returnValue = NotReachable;
+    SCNetworkReachabilityFlags flags;
+    
+    if (SCNetworkReachabilityGetFlags(self.reachabilityRef, &flags)) {
+        returnValue = [self networkStatusForFlags:flags];
+    }
+    
+    return returnValue;
+}
+
+@end
