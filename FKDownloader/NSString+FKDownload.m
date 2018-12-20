841b86b3882ba069250e3746de9bc9f501f92a77
norld <syfh@live.com>
2018-12-20 15:10:10 +0800

    URL 编码废弃系统方案, 使用 AFN 的解决方案

--- FKDownloader/NSString+FKDownload.m
+++ FKDownloader/NSString+FKDownload.m
@@ -7,6 +7,7 @@
 //
 
 #import "NSString+FKDownload.h"
+#import "NSArray+FKDownload.h"
 #import <CommonCrypto/CommonDigest.h>
 
 @implementation NSString (FKDownload)
@@ -110,7 +111,20 @@
 
 - (NSString *)encodeEscapedString {
     NSString *decode = [self decodeEscapedString];
-    return [decode stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+    NSRange range = [decode rangeOfString:@"?"];
+    if (range.location != NSNotFound) {
+        NSString *main = [decode substringToIndex:range.location];
+        NSString *query = [decode substringFromIndex:range.location + 1];
+        NSString *encode = [[[query componentsSeparatedByString:@"&"] map:^id(NSString *obj, NSUInteger idx) {
+            NSRange range = [obj rangeOfString:@"="];
+            NSString *key = [obj substringToIndex:range.location];
+            NSString *value = [obj substringFromIndex:range.location + 1];
+            return [NSString stringWithFormat:@"%@=%@", AFPercentEscapedStringFromString(key), AFPercentEscapedStringFromString(value)];
+        }] componentsJoinedByString:@"&"];
+        return [NSString stringWithFormat:@"%@?%@", main, encode];
+    } else {
+        return self;
+    }
 }
 
 // 递归式 URL Decode
@@ -121,6 +135,50 @@
     } else {
         return decode;
     }
+}
+
+/** AFNetworking 借用
+ Returns a percent-escaped string following RFC 3986 for a query string key or value.
+ RFC 3986 states that the following characters are "reserved" characters.
+ - General Delimiters: ":", "#", "[", "]", "@", "?", "/"
+ - Sub-Delimiters: "!", "$", "&", "'", "(", ")", "*", "+", ",", ";", "="
+ 
+ In RFC 3986 - Section 3.4, it states that the "?" and "/" characters should not be escaped to allow
+ query strings to include a URL. Therefore, all "reserved" characters with the exception of "?" and "/"
+ should be percent-escaped in the query string.
+ - parameter string: The string to be percent-escaped.
+ - returns: The percent-escaped string.
+ */
+NSString * AFPercentEscapedStringFromString(NSString *string) {
+    static NSString * const kAFCharactersGeneralDelimitersToEncode = @":#[]@"; // does not include "?" or "/" due to RFC 3986 - Section 3.4
+    static NSString * const kAFCharactersSubDelimitersToEncode = @"!$&'()*+,;=";
+    
+    NSMutableCharacterSet * allowedCharacterSet = [[NSCharacterSet URLQueryAllowedCharacterSet] mutableCopy];
+    [allowedCharacterSet removeCharactersInString:[kAFCharactersGeneralDelimitersToEncode stringByAppendingString:kAFCharactersSubDelimitersToEncode]];
+    
+    // FIXME: https://github.com/AFNetworking/AFNetworking/pull/3028
+    // return [string stringByAddingPercentEncodingWithAllowedCharacters:allowedCharacterSet];
+    
+    static NSUInteger const batchSize = 50;
+    
+    NSUInteger index = 0;
+    NSMutableString *escaped = @"".mutableCopy;
+    
+    while (index < string.length) {
+        NSUInteger length = MIN(string.length - index, batchSize);
+        NSRange range = NSMakeRange(index, length);
+        
+        // To avoid breaking up character sequences such as 👴🏻👮🏽
+        range = [string rangeOfComposedCharacterSequencesForRange:range];
+        
+        NSString *substring = [string substringWithRange:range];
+        NSString *encoded = [substring stringByAddingPercentEncodingWithAllowedCharacters:allowedCharacterSet];
+        [escaped appendString:encoded];
+        
+        index += range.length;
+    }
+    
+    return escaped;
 }
 
 @end
