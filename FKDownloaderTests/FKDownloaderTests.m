9e99363ef2e5f866eb1c1422831706ea63e04bfe
norld <syfh@live.com>
2020-02-03 11:54:15 +0800

    添加取消所有请求逻辑的相关单元测试

--- FKDownloaderTests/FKDownloaderTests.m
+++ FKDownloaderTests/FKDownloaderTests.m
@@ -41,6 +41,7 @@
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [FKControl cancelAllRequest];
 }
 
 - (void)testCodingURL {
